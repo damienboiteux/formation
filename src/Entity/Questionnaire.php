501c5bcbaba8435e74a0934a489342dce704c23d
Damien Boiteux <contact@damienboiteux.com>
2023-05-12 19:38:52 +0200

    !!! matiere & questionnaire entities corrigé !!!

--- src/Entity/Questionnaire.php
+++ src/Entity/Questionnaire.php
@@ -17,8 +17,13 @@ class Questionnaire
     #[ORM\Column(length: 255)]
     private ?string $titre = null;
 
-    // "Oublier pendant le live :
+    // "Oublié pendant le live :
     // #[ORM\Column(length: 255)]
+    // cet oubli avait pour conséquence de ne pas générer la colonne matiere_id dans la table questionnaire
+    // et donc de ne pas pouvoir faire le lien entre les deux entités
+    // mais également de ne pas générer la contrainte de clé étrangère
+    // Voir migration Version20230512173302.php
+    
     #[ORM\ManyToOne(targetEntity: Matiere::class, inversedBy: 'questionnaires')]
     private ?Matiere $matiere = null;
 
