adfdd02c0fbdfc671619b01121c300822b1aec2f
Damien Boiteux <contact@damienboiteux.com>
2023-05-19 18:28:58 +0200

    Create Matiere added

---
+++ src/Form/MatiereType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Matiere;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class MatiereType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle', TextType::class,['label'=>'Libellé de la matière'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Matiere::class,
+        ]);
+    }
+}
